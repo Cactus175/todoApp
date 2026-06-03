fb16f80fcabcb4c2aafa2c9cd47f48b27d76556e
MSu <maksimsukhov05@gmail.com>
2026-06-03 19:10:02 +0300

    init

---
+++ TodoApp/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-amber-900 w-1/2 h-full'>
+      <h1 className="text-red-500 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dicta quidem repellendus, vel numquam voluptatem? Omnis velit officiis nulla aliquid, non aliquam laudantium! Distinctio dolorem modi laudantium rerum aliquam quis?</h1>
+    </div>
+  )
+}
+
+export default App
