5b96b2ba7d4343cccc790335d37383ff510b40da
MSu <maksimsukhov05@gmail.com>
2026-06-04 17:01:53 +0300

    Layout + Routes

--- TodoApp/src/App.tsx
+++ TodoApp/src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { Link, NavLink, Route, Routes, BrowserRouter } from 'react-router-dom'
+import './index.css'
+import Tasks from './pages/Tasks'
+import Layout from './assets/Layout'
 
 function App() {
+
+
   return (
-    <div className='bg-amber-900 w-1/2 h-full'>
-      <h1 className="text-red-500 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dicta quidem repellendus, vel numquam voluptatem? Omnis velit officiis nulla aliquid, non aliquam laudantium! Distinctio dolorem modi laudantium rerum aliquam quis?</h1>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route path='/Tasks' element={<Layout><Tasks /></Layout>} />
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
 
---
+++ TodoApp/src/pages/Tasks.tsx
@@ -0,0 +1,5 @@
+function Tasks(){
+    return(
+        <div>test</div>
+    );
+}export default Tasks;
